4fe7b21624b2c744cd6f050ce6d67f6363edd812
lwr <a900240@gmail.com>
2024-06-19 12:20:08 +0800

    3110

---
+++ leetcode/Easy/3110. Score of a String/3110.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int res = 0;
+        for(int i=1; i<s.length(); i++){
+            res += Math.abs(s.charAt(i) - s.charAt(i-1));
+        }
+
+        return res;
+    }
+}
+
+// TC: O(n)
+// SC: O(1)
